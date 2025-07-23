d1b927062a1ffe17c0a623d9507e74c208bd7afd
Sam Foster <slfotg@gmail.com>
2025-07-23 12:50:51 -0500

    return pyarray instead of vec

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Parallel Prime Sieve for Python
 ```py
 >>> from par_sieve import get_primes
 >>> get_primes(100)
-[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+array([ 2,  3,  5,  7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
+       61, 67, 71, 73, 79, 83, 89, 97], dtype=uint64)
 >>> len(get_primes(1000000000))
 50847534
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
 use bitvec::prelude::*;
+use numpy::{ndarray::Dim, PyArray};
 use pyo3::prelude::*;
 use rayon::prelude::*;
 
-pub fn add_mod(a: usize, b: usize, m: usize) -> usize {
+pub fn add_mod(a: u64, b: u64, m: u64) -> u64 {
     ((a % m) + (b % m)) % m
 }
 
-pub fn sub_mod(a: usize, b: usize, m: usize) -> usize {
+pub fn sub_mod(a: u64, b: u64, m: u64) -> u64 {
     add_mod(a, m - (b % m), m)
 }
 
 #[pyfunction]
-pub fn get_primes(n: usize) -> PyResult<Vec<usize>> {
+pub fn get_primes(py: Python<'_>, n: usize) -> PyResult<Bound<'_, PyArray<u64, Dim<[usize; 1]>>>> {
     let n_len = ((n - 1) >> 1) + 1;
     let r = n.isqrt();
     let r_len = ((r - 1) >> 1) + 1;
@@ -32,13 +33,14 @@ pub fn get_primes(n: usize) -> PyResult<Vec<usize>> {
     let small_primes = [2]
         .into_iter()
         .chain(small.iter_ones().map(|x| (x << 1) ^ 1))
+        .map(|x| x as u64)
         .collect::<Vec<_>>();
 
     let chunk_size = 800_000;
 
     // If we don't need to sieve beyond sqrt(n), just return small primes
     if r_len >= n_len {
-        return Ok(small_primes);
+        return Ok(PyArray::from_vec(py, small_primes));
     }
 
     // Prepare chunk starts (just plain usize values, no Python objects)
@@ -58,12 +60,12 @@ pub fn get_primes(n: usize) -> PyResult<Vec<usize>> {
                     // Sieve this chunk using small primes
                     for &p in &small_primes[1..] {
                         // Skip 2, start from 3
-                        if p * p > end {
+                        if p * p > end as u64 {
                             break;
                         }
-                        let j = sub_mod((p * p - 1) >> 1, start, p);
-                        for j in (j..chunk.len()).step_by(p) {
-                            chunk.set(j, false);
+                        let j = sub_mod((p * p - 1) >> 1, start as u64, p);
+                        for j in (j..chunk.len() as u64).step_by(p as usize) {
+                            chunk.set(j as usize, false);
                         }
                     }
 
@@ -71,9 +73,10 @@ pub fn get_primes(n: usize) -> PyResult<Vec<usize>> {
                     chunk
                         .iter_ones()
                         .map(|x| ((x + start) << 1) ^ 1)
+                        .map(|x| x as u64)
                         .collect::<Vec<_>>()
                 })
-                .collect::<Vec<usize>>()
+                .collect::<Vec<_>>()
         })
     });
 
@@ -81,7 +84,8 @@ pub fn get_primes(n: usize) -> PyResult<Vec<usize>> {
     let mut all_primes = small_primes;
     all_primes.extend(large_primes);
 
-    Ok(all_primes)
+    let x = PyArray::from_vec(py, all_primes);
+    Ok(x)
 }
 
 /// A Python module implemented in Rust.
